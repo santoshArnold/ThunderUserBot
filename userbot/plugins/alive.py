776b8552669f04ab619b2cb257f71e97e958f1c3
santoshArnold <62205288+santoshArnold@users.noreply.github.com>
2020-04-30 19:34:16 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,7 +13,7 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`WE ARE ANONYMOUS USING THUNDER SECURITY`**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Anonymous_290\n"
-                     "`Bot created by:` [Anonymous_290](tg://user?id=1001239766297)\n"
+                     "`Bot created by:` [Anonymous_290](tg://user?id=719877937)\n"
                      "`Database Status: Databases functioning normally! The Secutity Is Stronger Connected To CTOS\n\nAlways with you Sir Waiting For Your Command\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      "[Deploy this userbot Now](https://github.com/santoshArnold/ThunderUserBot)")
